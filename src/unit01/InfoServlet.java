05d7ed57b1b8f2609d743b12dab5d8224ae82de7
Hyunfor <navel5148@gmail.com>
2023-03-20 20:36:39 +0900

    post 방식으로 한글 출력

---
+++ src/unit01/InfoServlet.java
@@ -0,0 +1,59 @@
+package unit01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InfoServlet
+ */
+@WebServlet("/InfoServlet")
+public class InfoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InfoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		
+		String name = request.getParameter("name");
+		String addr = request.getParameter("addr");
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>Hello</title></head>");
+		out.println("<body>");
+		out.println("<h1>유저가 입력한 정보입니다.</h1>");
+		out.println("이름: ");
+		out.println(name);
+		out.println("주소: ");
+		out.println(addr);
+		
+		out.println("<br><a href='javascript:history.go(-1)'>다시</a>");
+		out.println("</body>");
+		out.println("</html>");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
